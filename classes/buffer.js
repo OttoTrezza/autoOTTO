ce6eb37253997228947fd6529c87fca9fb379ea1
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-01 02:25:28 -0300

    serie 1c

--- classes/buffer.js
+++ classes/buffer.js
@@ -115,9 +115,9 @@ class ValorControl {
         // console.log('ultimos4', ultimos4);
         // console.log('ultimos4[0]', ultimos4[0]);
         // console.log('ultimos4[0].beta1', ultimos4[0].beta1);
-        let ultimos4val = parseInt(ultimos4[0].beta1);
-        let ultimos4vala = parseInt(ultimos4[3].beta1);
-        if (ultimos4val > ultimos4vala) {
+        // let ultimos4val = parseInt(ultimos4[0].beta1);
+        // let ultimos4vala = parseInt(ultimos4[3].beta1);
+        if (ultimos4[0].beta1 != ultimos4[3].beta1) {
             console.log('es mayor');
             this.codigoEvento = 1;
             return;
