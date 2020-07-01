9b414726ca3a2888ec6ab60af87ef437f98d656b
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-01 09:24:52 -0300

    serie6f

--- sockets/socket.js
+++ sockets/socket.js
@@ -154,6 +154,41 @@ exports.ElSarmiento = (cliente) => {
         // cliente.emit('mensaje-nuevo-auto', msg1);
         cliente.to('Juegos').emit('ElSarmiento-nuevo', msg);
         cliente.emit('ElSarmiento-nuevo', msg);
+        let dispos = valorControl.getDispositivos();
+
+        let va0 = valorControl.getUltimoValor(dispos[0]);
+        const paya = {
+            de: dispos[0],
+            sala: 'Juegos',
+            beta1: va0.beta1,
+            gamma1: va0.gamma1,
+            alpha1: va0.alpha1
+        };
+        cliente.to('Juegos').emit('Dispo1', paya);
+        cliente.emit('Dispo1', paya);
+
+        let va1 = valorControl.getUltimoValor(dispos[1]);
+        const paya1 = {
+            de: dispos[1],
+            sala: 'Juegos',
+            beta1: va1.beta1,
+            gamma1: va1.gamma1,
+            alpha1: va1.alpha1
+        };
+        cliente.to('Juegos').emit('Dispo2', paya1);
+        cliente.emit('Dispo2', paya1);
+
+        let va2 = valorControl.getUltimoValor(dispos[2]);
+        const paya2 = {
+            de: dispos[2],
+            sala: 'Juegos',
+            beta1: va2.beta1,
+            gamma1: va2.gamma1,
+            alpha1: va2.alpha1
+        };
+        cliente.to('Juegos').emit('Dispo3', paya2);
+        cliente.emit('Dispo3', paya2);
+
         // cliente.emit('ElSarmiento1-nuevo', msg1);
         console.log(payload.de, 'ha enviado esto', msg);
         let codEv = valorControl.getCodigoEvento();
