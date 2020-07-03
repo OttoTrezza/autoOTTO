f47dc33e7b20244f3b429bae95e3cf441e5bf1f4
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-03 00:26:03 -0300

    Serie 12 H

--- classes/buffer.js
+++ classes/buffer.js
@@ -80,7 +80,7 @@ class ValorControl {
         if (this.valores.length === 0) { //VERIFICA QUE HAYAN TICKETS PENDIENTES DE ATENDER
             return 'No hay Valores';
         }
-        let dispositivor = this.getUltimoValor(dispositivo).dispositivo;
+        // let dispositivor = this.getUltimoValor(dispositivo).dispositivo;
         let beta1Valor = this.getUltimoValor(dispositivo).beta1; // EXTRAIGO EL NUMERO PARA ROMPER LA RELACION QUE TIENE JSCRIPT CON QUE TODOS LOS OBJETOS SON PASADOS POR REFERENCIA
         let gamma1Valor = this.getUltimoValor(dispositivo).gamma1;
         let alpha1Valor = this.getUltimoValor(dispositivo).alpha1;
