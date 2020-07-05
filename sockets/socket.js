5d21201f888317c1edd38cb8c1084baafe6a202c
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-05 06:18:33 -0300

    serie 2 b

--- sockets/socket.js
+++ sockets/socket.js
@@ -115,7 +115,7 @@ exports.ElSarmiento = (cliente) => {
 
         let va0 = valorControl.getUltimoValor();
         let dispoConec = valorControl.getDispositivosConectados();
-        let dispoConecult4 = valorControl.getDispositivosConectadosporPos(0);
+        //  let dispoConecult4 = valorControl.getDispositivosConectadosporPos(0);
         // let dispoConecult41 = valorControl.getDispositivosConectadosporPos(1);
         console.log('DISPOCONEC', dispoConec);
         console.log('dispoConecult4 0', dispoConecult4);
