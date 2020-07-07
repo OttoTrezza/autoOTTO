5a2b6e8d4d20d2873844fcb056cd9570c4372871
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-07 04:50:31 -0300

    serie 2 G

--- classes/buffer.js
+++ classes/buffer.js
@@ -77,10 +77,8 @@ class ValorControl {
     siguiente(pos1, dispo1, beta1, gamma1, alpha1, accelerationx1, accelerationy1, accelerationz1, accelerationincludinggravityx1, accelerationincludinggravityy1, accelerationincludinggravityz1, rotationratebeta1, rotationrategamma1, rotationratealpha1) {
         this.ultimo = this.ultimo + 1;
 
-        let ind = this.possi.findIndex((dispo1) => {
+        let ind = this.possi.findIndex((element) => element === dispo1);
 
-            return dispo1;
-        });
         pos1 = ind;
         console.log('pos1, ind, dispo1', pos1, ind, dispo1);
 
