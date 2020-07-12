365623dd6dfe78c03b54bde8a69ccd64239cf00e
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-12 03:08:04 -0300

    ultimos4 serie 1 I

--- classes/buffer.js
+++ classes/buffer.js
@@ -82,6 +82,7 @@ class ValorControl {
             let valor = new Valor(ind, dispo1, alpha1, beta1, gamma1);
             this.valor = valor;
             this.valores.push(valor);
+            this.ultimos4.push(ind);
             this.grabarArchivo();
         } else {
             let valor = new Valor(ind, dispo1, alpha1, beta1, gamma1);
@@ -111,9 +112,13 @@ class ValorControl {
         let atenderValor = new Valor(pos1Valor, dispo1Valor, alpha1Valor, beta1Valor, gamma1Valor); // ,  accelerationx1Valor, accelerationy1Valor, accelerationz1Valor, accelerationincludinggravityx1Valor, accelerationincludinggravityy1Valor, accelerationincludinggravityz1Valor, rotationratebeta1Valor, rotationrategamma1Valor, rotationratealpha1Valor
         this.possi[pos1Valor] = [];
         this.possi[pos1Valor].unshift(atenderValor);
+        this.ultimos4[pos1Valor] = this.possi[pos1Valor];
+        this.ultimos4[pos1Valor].unshift(atenderValor);
         if (this.possi[pos1Valor].length > 4) { // VERIFICO QUE SIEMPRE SEAN 4
             this.possi[pos1Valor].splice(-1, 1);
         }
+
+
         let lachot = this.possi[pos1Valor];
         this.ultimos4 = lachot;
         this.possi[pos1Valor] = dispo1Valor;
