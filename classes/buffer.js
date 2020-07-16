11fa8064364a589135d1e918f0717083284ab9f4
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-07-16 03:08:32 -0300

    con tiempo serie 1 I

--- classes/buffer.js
+++ classes/buffer.js
@@ -107,9 +107,9 @@ class ValorControl {
             let Ginterval = gamma1 - ultValxdispo.gamma1;
 
             // calculando la pendiente de las 3 variables en func' del tiempo. m=(alpha-alpha')/(tiempo - tiempo')
-            alpha1 = Ainterval / Tinterval;
-            beta1 = Binterval / Tinterval;
-            gamma1 = Ginterval / Tinterval;
+            alpha1 = Math.round(Ainterval / Tinterval);
+            beta1 = Math.round(Binterval / Tinterval);
+            gamma1 = Math.round(Ginterval / Tinterval);
             let valor = new Valor(ind, dispo1, alpha1, beta1, gamma1, tiempo);
             // this.valores.push(valor);
             this.valor = valor;
